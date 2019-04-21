835ba5d3d3e2a4649729f2404d27ef147bab0715
hbandesh <hbandesh@sapient.com>
2019-04-22 01:03:19 +0530

    Comiting boilerplate of React and platform folders of react native

---
+++ webpack/paths.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  root: path.resolve(__dirname, '../'),
+  outputPath: path.resolve(__dirname, '../', 'build'),
+  entryPath: path.resolve(__dirname, '../', 'index.web.js'),
+  templatePath: path.resolve(__dirname, '../', 'web/index.html'),
+  imagesFolder: 'images',
+  fontsFolder: 'fonts',
+  cssFolder: 'css',
+  jsFolder: 'js',
+};
---
+++ webpack/webpack.common.js
@@ -0,0 +1,108 @@
+const webpack = require('webpack');
+const convert = require('koa-connect');
+const history = require('connect-history-api-fallback');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const commonPaths = require('./paths');
+
+module.exports = {
+  entry: commonPaths.entryPath,
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          emitWarning: process.env.NODE_ENV !== 'production',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+                useBuiltIns: 'usage',
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/transform-react-constant-elements',
+            '@babel/transform-react-inline-elements',
+            'transform-react-remove-prop-types',
+            'transform-react-pure-class-to-function',
+            '@babel/plugin-transform-runtime',
+            'react-hot-loader/babel',
+
+            // Stage 2 https://github.com/babel/babel/tree/master/packages/babel-preset-stage-2
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+
+            // Stage 3
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-import-meta',
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            '@babel/plugin-proposal-json-strings',
+          ],
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.imagesFolder,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.fontsFolder,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  serve: {
+    add: app => {
+      app.use(convert(history()));
+    },
+    content: commonPaths.entryPath,
+    dev: {
+      publicPath: commonPaths.outputPath,
+    },
+    open: true,
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.css', '.scss'],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath,
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async',
+    }),
+  ],
+};
