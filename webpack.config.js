cd4c0b0eb3a6565c2d9f46c4872835de01bb004d
Michał Matczuk <michal.matczuk@venturedevs.com>
2020-10-28 22:17:58 +0100

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpackMerge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack/webpack.common');
 
 const envs = {
@@ -8,4 +8,4 @@ const envs = {
 /* eslint-disable global-require,import/no-dynamic-require */
 const env = envs[process.env.NODE_ENV || 'development'];
 const envConfig = require(`./webpack/webpack.${env}.js`);
-module.exports = webpackMerge(common, envConfig);
+module.exports = merge(common, envConfig);
